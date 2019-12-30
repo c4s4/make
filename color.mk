58218583e070ed163fe1ecadd86d812fd4a1ca9a
Michel Casabianca <casa@sweetohm.net>
2019-12-30 14:55:47 +0100

    Initial import

---
+++ color.mk
@@ -0,0 +1,21 @@
+# normal colors
+bla="\\033[30m"
+red="\\033[31m"
+gre="\\033[32m"
+yel="\\033[33m"
+blu="\\033[34m"
+mag="\\033[35m"
+cya="\\033[36m"
+whi="\\033[37m"
+# bright colors
+BLA="\\033[90m"
+RED="\\033[91m"
+GRE="\\033[92m"
+YEL="\\033[93m"
+BLU="\\033[94m"
+MAG="\\033[95m"
+CYA="\\033[96m"
+WHI="\\033[97m"
+# end color
+end="\\033[0m"
+END="\\033[0m"
