86d046d534f55ba63dbf5bfe66865b1c77edd13f
Michel Casabianca <casa@sweetohm.net>
2020-02-14 22:41:58 +0100

    Using make-help in help target

--- help.mk
+++ help.mk
@@ -1,2 +1,3 @@
+.PHONY: help
 help: # Print help on Makefile
-	@grep '^[^.#]\+:\s\+.*#' Makefile | sed "s/\(.\+\):\s*\(.*\) #\s*\(.*\)/`printf "\033[93m"`\1`printf "\033[0m"`	\3 [\2]/" | expand -t20
+	@make-help
