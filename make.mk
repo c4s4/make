8ced3f60d017d262531f09dd04617e0759d3ee2d
Michel Casabianca <casa@sweetohm.net>
2020-05-07 15:14:00 +0200

    Fixed makeid and added makeup

--- make.mk
+++ make.mk
@@ -1,15 +1,23 @@
 # Makefile for Make (https://github.com/c4s4/make)
 
-MAKE_ID=78076a93b1c9051fa64657cc48836c682ea9e3f4
+MAKE_ID=PUT_HERE_COMMIT_ID_OF_MAKE_REPO
 MAKE_DIR=.make
+MAKE_REPO=~/.make
+MAKE_URL=git@github.com:c4s4/make.git
 
-make: # Build makefile dependencies
-	@echo "$(YEL)Building makefile dependencies$(END)"
+make: # Build makefile dependencies for given commit ID
+	@echo "$(YEL)Building makefile dependencies for given commit ID$(END)"
 	@rm -rf $(MAKE_DIR)
-	@git clone git@github.com:c4s4/make.git $(MAKE_DIR)
+	@git clone $(MAKE_URL) $(MAKE_DIR)
 	@cd $(MAKE_DIR) && git checkout $(MAKE_ID)
 
-id: # Print commit id
-	@echo "$(YEL)Printing commit id$(END)"
-	@ID=`git rev-parse HEAD`; \
+makeid: # Print current make repository commit ID
+	@echo "$(YEL)Printing current commit ID$(END)"
+	@cd $(MAKE_REPO); \
+	ID=`git rev-parse HEAD`; \
 	echo "Commit ID: $${ID}"
+
+makeup: # Update make repository
+	@echo "$(YEL)Updating make repository$(END)"
+	@cd $(MAKE_REPO); \
+	git pull --rebase
