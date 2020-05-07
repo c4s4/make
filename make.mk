c8addc1e47e6469c2f4629495fd15c79603de483
Michel Casabianca <casa@sweetohm.net>
2020-05-07 15:24:19 +0200

    Fixed make parent makefile

--- make.mk
+++ make.mk
@@ -1,23 +1,22 @@
 # Makefile for Make (https://github.com/c4s4/make)
 
-MAKE_ID=PUT_HERE_COMMIT_ID_OF_MAKE_REPO
+MAKE_ID=HEAD
 MAKE_DIR=.make
-MAKE_REPO=~/.make
 MAKE_URL=git@github.com:c4s4/make.git
 
 make: # Build makefile dependencies for given commit ID
 	@echo "$(YEL)Building makefile dependencies for given commit ID$(END)"
 	@rm -rf $(MAKE_DIR)
 	@git clone $(MAKE_URL) $(MAKE_DIR)
-	@cd $(MAKE_DIR) && git checkout $(MAKE_ID)
+	@cd $(MAKE_DIR); \
+	git checkout $(MAKE_ID)
 
 makeid: # Print current make repository commit ID
 	@echo "$(YEL)Printing current commit ID$(END)"
-	@cd $(MAKE_REPO); \
-	ID=`git rev-parse HEAD`; \
+	@ID=`git ls-remote $(MAKE_URL) refs/heads/master`; \
 	echo "Commit ID: $${ID}"
 
-makeup: # Update make repository
-	@echo "$(YEL)Updating make repository$(END)"
-	@cd $(MAKE_REPO); \
-	git pull --rebase
+makeup: # Update make repository to commit ID
+	@echo "$(YEL)Updating make repository to commit ID$(END)"
+	@cd $(MAKE_DIR); \
+	git checkout $(MAKE_ID)
