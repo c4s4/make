6c94b169ce7210b77f80b256b3eb45b74bd53f18
Michel Casabianca <casa@sweetohm.net>
2020-06-10 10:45:07 +0200

    Added updatesub target

--- git.mk
+++ git.mk
@@ -35,3 +35,8 @@ squash: # Squash branch and merge on master
 tags: # List tags sorted by version
 	@echo "$(YEL)Listing tags sorted by version$(END)"
 	@git tag | sort -V
+
+.PHONY: updatesub
+updatesub: # Update submodules
+	@echo "$(RED)Updating submodules$(END)"
+	@git submodule update --remote
