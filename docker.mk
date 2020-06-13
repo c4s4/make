ffdc2d06e42cd79b7bb574df6298bbbb48c9b833
Michel Casabianca <casa@sweetohm.net>
2020-06-14 00:15:00 +0200

    Added docker-start and docker-stop targets

--- docker.mk
+++ docker.mk
@@ -1,11 +1,23 @@
 # Parent makefile for Build (https://github.com/c4s4/make)
 
-.PHONY: docker
-docker: # Ensure docker is started
-	@echo "$(YEL)Ensuring docker is started$(END)"
+.PHONY: docker-running
+docker-running: # Ensure docker is running
+	@echo "$(YEL)Ensuring docker is running$(END)"
 	@if [ `systemctl show --property ActiveState docker` != "ActiveState=active" ]; then \
 		echo "Starting docker"; \
 		sudo systemctl start docker; \
 	else \
 		echo "Docker already running"; \
 	fi
+
+docker-build: docker-running # Build and start docker platform
+	@echo "$(YEL)Building and starting docker platform$(END)"
+	@docker-compose up --build --force-recreate --remove-orphans --detach
+
+docker-start: # Start docker platform
+	@echo "$(YEL)Starting docker platform$(END)"
+	@docker-compose start
+
+docker-stop: # Stop docker platform
+	@echo "$(YEL)Stopping docker platform$(END)"
+	@docker-compose stop
