4bcdcf997c2e2eddb1252aacc67eaecf84147d8f
Michel Casabianca <casa@sweetohm.net>
2020-06-14 00:20:23 +0200

    Changed docker prefix for doc-

--- docker.mk
+++ docker.mk
@@ -1,7 +1,7 @@
 # Parent makefile for Build (https://github.com/c4s4/make)
 
-.PHONY: docker-running
-docker-running: # Ensure docker is running
+.PHONY: doc-running
+doc-running: # Ensure docker is running
 	@echo "$(YEL)Ensuring docker is running$(END)"
 	@if [ `systemctl show --property ActiveState docker` != "ActiveState=active" ]; then \
 		echo "Starting docker"; \
@@ -10,17 +10,17 @@ docker-running: # Ensure docker is running
 		echo "Docker already running"; \
 	fi
 
-.PHONY: docker-build
-docker-build: docker-running # Build and start docker platform
+.PHONY: doc-build
+doc-build: docker-running # Build and start docker platform
 	@echo "$(YEL)Building and starting docker platform$(END)"
 	@docker-compose up --build --force-recreate --remove-orphans --detach
 
-.PHONY: docker-start
-docker-start: # Start docker platform
+.PHONY: doc-start
+doc-start: # Start docker platform
 	@echo "$(YEL)Starting docker platform$(END)"
 	@docker-compose start
 
-.PHONY: docker-stop
-docker-stop: # Stop docker platform
+.PHONY: doc-stop
+doc-stop: # Stop docker platform
 	@echo "$(YEL)Stopping docker platform$(END)"
 	@docker-compose stop
