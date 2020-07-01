a759e713a377e5dc4f7b2dff1a65243a58f8ed92
Michel Casabianca <casa@sweetohm.net>
2020-07-01 11:01:21 +0200

    Added title function in help parent makefile

--- help.mk
+++ help.mk
@@ -1,5 +1,9 @@
 # Parent makefile for Help (https://github.com/c4s4/make)
 
+define title =
+	@echo "$(YEL)$(shell make-desc $@)$(END)"
+endef
+
 .PHONY: help
 help: # Print help on Makefile
 	@make-help
