ebf858c9c0be8a60c93e215d6354f35823d432a7
Michel Casabianca <casa@sweetohm.net>
2020-10-10 17:30:21 +0200

    fixes

--- django.mk
+++ django.mk
@@ -2,18 +2,22 @@
 
 SERVER_PORT=0:8000
 
+.PHONY: dj-runserver
 dj-runserver: # Run Django development server
 	$(title)
 	$(PYTHON) manage.py runserver $(SERVER_PORT)
 
+.PHONY: dj-migrations
 dj-migrations: # Generate migration scripts
 	$(title)
 	$(PYTHON) manage.py makemigrations
 
+.PHONY: dj-migrate
 dj-migrate: # Run migration scripts
 	$(title)
 	$(PYTHON) manage.py migrate
 
+.PHONY: dj-shell
 dj-shell: # Run Django shell
 	$(title)
 	$(PYTHON) manage.py shell
